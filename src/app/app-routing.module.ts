2b0e2c472c1354ed84ed518da88d85820b87c373
Chris Mastel <chris@craggy.ca>
2022-05-19 05:50:21 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeopleComponent } from './people/people.component';
+import { GroupsComponent } from './groups/groups.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/homepage', pathMatch: 'full'},
+  { path: 'people', component: PeopleComponent },
+  { path: 'groups', component: GroupsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
