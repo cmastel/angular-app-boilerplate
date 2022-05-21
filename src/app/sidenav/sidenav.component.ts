ef3de045a06a6fb43bb0467e629890510c8b77d9
Chris Mastel <chris@craggy.ca>
2022-05-21 07:55:49 -0600

    responsive sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Component, OnDestroy} from '@angular/core';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.sass']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnDestroy {
+  destroyed = new Subject<void>();
+  isSmallScreen: boolean = false;
 
-  constructor() { }
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(result => {
+        for (const query of Object.keys(result.breakpoints)) {
+          if (result.breakpoints[query]) {
+            this.isSmallScreen = breakpointObserver.isMatched('(max-width: 599px)');
+          }
+        }
+      });
+  }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
 }
